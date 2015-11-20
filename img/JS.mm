fe66d2bd6fad7efb301bca5ed75e239db072a92e
Janardan Revuru <janardan.revuru@gmail.com>
2015-11-20 20:29:44 +0530

    Final version of presentation at osiday 2015, Nov 20

---
+++ img/JS.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1447935746962" ID="ID_136008982" MODIFIED="1447936042874" TEXT="JavaScript">
+<node CREATED="1447935809312" ID="ID_1404700478" MODIFIED="1447935817564" POSITION="right" TEXT="UI"/>
+<node CREATED="1447935821637" ID="ID_367962025" MODIFIED="1447935830016" POSITION="right" TEXT="Data Interchange (JSON)"/>
+<node CREATED="1447935830962" ID="ID_1366935417" MODIFIED="1447935837755" POSITION="right" TEXT="Databases (object store)"/>
+<node CREATED="1447935838438" ID="ID_66470908" MODIFIED="1447935844756" POSITION="right" TEXT="Backend (business logic)"/>
+</node>
+</map>
